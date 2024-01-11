6c740543561a4485de5456e5c99ff2265c84a6f2
Michael Tavarez-Feliciano <mtavarezfeliciano@gmail.com>
2024-01-10 20:25:42 -0500

    updated calc

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-var rs = require("readline-sync");
+const rs = require("readline-sync");
 
-console.log("Welcome, Nicholas :^)");
+console.log("Welcome, Nicholas, or Andrei. :^)");
+
+let validSymbols = ["+", "-", "*", "/"];
+let isValidOperation = false;
+
+let operation;
+
+//check if you using an actual operation symbol or naw
+
+while (!isValidOperation) {
+  operation = rs.question("What operation would you like to perform?");
+  if (validSymbols.includes(operation)) {
+    isValidOperation = true;
+  } else {
+    console.log("That is not a valid operation. Please try again.");
+  }
+}
+
+const getNum = (str) => {
+  while(true) {
+    const num = rs.question(`What is the ${str} number? `);
+
+    if (!isNaN(num) && num !== '') {
+      console.log(num);
+      return Number.parseInt(num);
+    } else {
+      console.log('That is not a valid number.');
+    }
+  }
+};
+
+const num1 = getNum('first');
+const num2 = getNum('second');
 
 //the functions for the calculations
 
@@ -17,41 +49,6 @@ function div(num1, num2) {
   return num1 / num2;
 }
 
-let validSymbols = ["+", "-", "*", "/"];
-let isValidOperation = false;
-
-let operation;
-let num1;
-let num2;
-
-//check if you using an actual operation symbol or naw
-
-while (!isValidOperation) {
-  operation = rs.question("What operation would you like to perform?");
-  if (validSymbols.includes(operation)) {
-    isValidOperation = true;
-  } else {
-    console.log("That is not a valid operation. Please try again.");
-  }
-}
-// im sorry for all the notes its 1:43am at the moment and im going crazy.
-
-num1 = parseInt(rs.question("Please enter the first number: "));
-
-if (isNaN(num1)){
-    console.log('That is not a number.');
-    process.exit();
-}
-
-num2 = parseInt(rs.question("Please enter the second number: "));
-
-if (isNaN(num2)){
-    console.log('That is not a number.');
-    process.exit();
-}
-
-//im going to use and abuse this exit function oh my god.
-
 if (operation == "+") {
   console.log("The result is " + add(num1, num2));
 } else if (operation == "-") {
@@ -61,3 +58,5 @@ if (operation == "+") {
 } else if (operation == "/") {
   console.log("The result is " + div(num1, num2));
 }
+
+
