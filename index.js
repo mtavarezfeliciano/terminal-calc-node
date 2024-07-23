5adb50b7f91c6be34a946680ee05c6061a39084a
Michael Tavarez-Feliciano <mtavarezfeliciano@gmail.com>
2024-07-23 01:16:41 -0400

    redo and cleaned up code, with commented out old code from previous submission

--- index.js
+++ index.js
@@ -1,62 +1,94 @@
 const rs = require("readline-sync");
 
-console.log("Welcome, Nicholas, or Andrei. :^)");
+console.log("Welcome, this is a redo of an already approved assignment.");
 
-let validSymbols = ["+", "-", "*", "/"];
-let isValidOperation = false;
-
+const validSymbols = ["+", "-", "*", "/"];
+// let isValidOperation = false;
 let operation;
 
-//check if you using an actual operation symbol or naw
+// while (!isValidOperation) {
+//   operation = rs.question("What operation would you like to perform?");
+//   if (validSymbols.includes(operation)) {
+//     isValidOperation = true;
+//   } else {
+//     console.log("That is not a valid operation. Please try again.");
+//   }
+// }
 
-while (!isValidOperation) {
-  operation = rs.question("What operation would you like to perform?");
-  if (validSymbols.includes(operation)) {
-    isValidOperation = true;
-  } else {
-    console.log("That is not a valid operation. Please try again.");
-  }
-}
-
-const getNum = (str) => {
-  while(true) {
-    const num = rs.question(`What is the ${str} number? `);
-
-    if (!isNaN(num) && num !== '') {
-      console.log(num);
-      return Number.parseInt(num);
+const isValidOperation = () => {
+  let op;
+  while (true) {
+    op = rs.question("What operation would you like to perform?");
+    if (validSymbols.includes(op)) {
+      return op;
     } else {
-      console.log('That is not a valid number.');
+      console.log("not a valid operation");
     }
   }
 };
 
-const num1 = getNum('first');
-const num2 = getNum('second');
+// const getNum = (str) => {
+//   while(true) {
+//     const num = rs.question(`What is the ${str} number? `);
+
+//     if (!isNaN(num) && num !== '') {
+//       console.log(num);
+//       return Number.parseInt(num);
+//     } else {
+//       console.log('That is not a valid number.');
+//     }
+//   }
+// };
+
+const getNum = (str) => {
+  while (true) {
+    const num = rs.question(`What is the ${str} number? `);
+    if (!isNaN(num) && num !== "") {
+      return parseFloat(num);
+    } else {
+      console.log("not a valid number");
+    }
+  }
+};
+
+// const num1 = getNum('first');
+// const num2 = getNum('second');
+
+const operations = {
+  "+": (num1, num2) => num1 + num2,
+  "-": (num1, num2) => num1 - num2,
+  "*": (num1, num2) => num1 * num2,
+  "/": (num1, num2) => num1 / num2,
+};
 
 //the functions for the calculations
 
-function add(num1, num2) {
-  return num1 + num2;
-}
-function sub(num1, num2) {
-  return num1 - num2;
-}
-function multi(num1, num2) {
-  return num1 * num2;
-}
-function div(num1, num2) {
-  return num1 / num2;
-}
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// function sub(num1, num2) {
+//   return num1 - num2;
+// }
+// function multi(num1, num2) {
+//   return num1 * num2;
+// }
+// function div(num1, num2) {
+//   return num1 / num2;
+// }
 
-if (operation == "+") {
-  console.log("The result is " + add(num1, num2));
-} else if (operation == "-") {
-  console.log("The result is " + sub(num1, num2));
-} else if (operation == "*") {
-  console.log("The result is " + multi(num1, num2));
-} else if (operation == "/") {
-  console.log("The result is " + div(num1, num2));
-}
+// if (operation == "+") {
+//   console.log("The result is " + add(num1, num2));
+// } else if (operation == "-") {
+//   console.log("The result is " + sub(num1, num2));
+// } else if (operation == "*") {
+//   console.log("The result is " + multi(num1, num2));
+// } else if (operation == "/") {
+//   console.log("The result is " + div(num1, num2));
+// }
 
+operation = isValidOperation();
+const num1 = getNum('first');
+const num2 = getNum('second');
+const result = operations[operation](num1, num2);
 
+console.log(`The result is ${result}`);
